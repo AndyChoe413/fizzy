1a2476b16f6dc8457337ac9eb4f7b012a477a1bc
Andy Choe <Andrew.s.choe@gmail.com>
2021-02-22 14:59:57 -0500

    Finished Fizzy assignment

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 /******************************
  * YOUR CODE GOES RIGHT HERE! *
  ******************************/
+const fizzy = (num) => {
+  if (num % 3 !== 0 && num % 5 !== 0) return num
+  else if(num % 5 === 0 && num % 3 === 0) return 'FizzBuzz'
+  else if (num % 5 === 0 && num % 3 !== 0) return 'Buzz'
+  else return 'Fizz'
+}
 
 
 
 
 
-
-
+    // ✕ returns 'Fizz' if the given number is divisible by 3 and not 5. (2 ms)
+    // ✕ returns 'Buzz' if the given number is divisible by 5 and not 3. (1 ms)
+    // ✕ returns 'FizzBuzz' if the given number is divisible by both 3 and 5
+    // ✕ returns the number if the given number is divisible by neither 3 nor 5
 
 
 /**********************************
